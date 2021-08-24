ed84211a9aff44e03032f3cd64c54ccd189fa6d4
MadalapureVaishnavi <89457366+MadalapureVaishnavi@users.noreply.github.com>
2021-08-24 19:06:06 +0530

    Add files via upload

---
+++ ind11.java
@@ -0,0 +1,15 @@
+// Write a program in JAVA to print a welcome text in a separate line.   
+ 
+
+public class Welcome
+{
+	public static void main(String[] args)
+	 {
+		System.out.println("Print a welcome text in a separate line :");
+		
+			System.out.println("-----------------------------------------");
+			System.out.println("Welcome to");
+			
+				System.out.println("java programs.");
+	}
+}
